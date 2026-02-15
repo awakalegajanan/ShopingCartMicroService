dfba3e2e22b698c11f571d060314a1271e19f938
Gajanan Awakale <awakale.gajanan@live.in>
2026-02-15 10:11:20 +0530

    get products

---
+++ src/Services/Catelog/Catelog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catelog.API.Products.GetProducts
+{
+    //public record GetProductsRequest();
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products",
+                async (ISender sender) =>
+                {
+                    var query = new GetProductsQuery();
+                    var result = await sender.Send(query);
+                    var response = result.Adapt<GetProductsResponse>();
+                    return Results.Ok(response);
+                })
+                .WithName("GetProducts")
+                .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Retrieves a list of products")
+                .WithDescription("Retrieves a list of all products available in the catalog.");
+        }
+    }
+}
